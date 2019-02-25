4ff86daac017b5885cea2b228e927a8e255899e7
Shivanibhakta <s9bhakta@ucsd.edu>
2019-02-25 09:54:44 -0800

    Unit 2 Stuff plus the app pictures

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Shivani Bhakta on 2/24/19.
+//  Copyright © 2019 Shivani Bhakta. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
